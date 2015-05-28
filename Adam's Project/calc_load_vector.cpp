dda3286f77232ab108980e53d013b41376b918a6
asaverino1019 <asaverino1019@gmail.com>
2015-05-27 19:37:54 -0700

    Disobedient
    the load vector values are not updating (use plate.inp)

--- Adam's Project/calc_load_vector.cpp
+++ Adam's Project/calc_load_vector.cpp
@@ -6,17 +6,16 @@
 #include "calc_load_vector.h"
 using namespace std;
 
-calc_load_vector::calc_load_vector(ElementData &element, Controller &controller, calc_B_matrix &calc_B_matrix, double zeta, double eta)
+calc_load_vector::calc_load_vector(ElementData &element, Controller &controller, calc_B_matrix & calc_B_matrix, double zeta, double eta)
 {
 	//define component and solution vectors and matrices
 	NodeEqnMatrix = MatrixFunctions::allocateMatrix(16, 2);
 	double ForceVector[16];
+	double tempVector[16];
 	double LoadVector[16];
 	numPFs = controller.getPFs();
-
-	//RETRIEVE JACOBIAN DETERMINANT FROM CALC_B_MATRIX!!!!!!
 	detJacob = calc_B_matrix.getJacobianDet();
-	
+	/*
 	//define node equations
 	double N1 = (-1 / 4)*(1 - zeta)*(1 - eta)*(zeta + eta + 1);
 	double N2 = (1 / 2)*(1 - zeta*zeta)*(1 - eta);
@@ -60,27 +59,15 @@ calc_load_vector::calc_load_vector(ElementData &element, Controller &controller,
 	NodeEqnMatrix[14][1] = 0;
 	NodeEqnMatrix[15][0] = 0;
 	NodeEqnMatrix[15][1] = N8;
-	
+	*/
 	//populate force vector
-	ForceVector[0] = fx1;
-	ForceVector[1] = fy1;
-	ForceVector[2] = fx2;
-	ForceVector[3] = fy2;
-	ForceVector[4] = fx3;
-	ForceVector[5] = fy3;
-	ForceVector[6] = fx4;
-	ForceVector[7] = fy4;
-	ForceVector[8] = fx5;
-	ForceVector[9] = fy5;
-	ForceVector[10] = fx6;
-	ForceVector[11] = fy6;
-	ForceVector[12] = fx7;
-	ForceVector[13] = fy7;
-	ForceVector[14] = fx8;
-	ForceVector[15] = fy8;
+	for (int i = 0; i < 16;i++)
+	{
+		ForceVector[i] = 0;
+	}
 
 	//define element node array
-	int Node[8];
+	//int Node[8];
 	Node[0] = element.Node1;
 	Node[1] = element.Node2;
 	Node[2] = element.Node3;
@@ -91,26 +78,33 @@ calc_load_vector::calc_load_vector(ElementData &element, Controller &controller,
 	Node[7] = element.Node8;
 
 	//apply point forces to force vector
-	for (int i = 0; i < 8; i++)
+	for (int j = 0; j < 8; j++)
 	{
-		for (int j = 0; j < numPFs; j++)
+		for (int k = 0; k < numPFs; k++)
 		{
-			if (Node[i] == controller.getPFNode(j))
+			if(Node[j] == controller.getPFNode(k))
 			{
-				if (strcmp(controller.getPFdirection(j), "UX") == 0)
+				ForceVector[j * 2 - 1] = controller.getPFNode(k);
+				if (strcmp(controller.getPFdirection(k), "FX") == 0)
 				{
-					ForceVector[i * 2 - 1] = controller.getPFvalue(j);
+					ForceVector[j * 2 - 1] = ForceVector[j*2-1]+controller.getPFvalue(k);
 				}
-				if (strcmp(controller.getPFdirection(j), "UY") == 0)
+				else if (strcmp(controller.getPFdirection(k), "FY") == 0)
 				{
-					ForceVector[i * 2] = controller.getPFvalue(j);
+					ForceVector[j * 2] = controller.getPFvalue(k);
 				}
 			}
+
 		}
 	}
 
-	//combine terms
-	MatrixFunctions::multiply(2, 2, NodeEqnMatrix, ForceVector, LoadVector);
+	//multiply vectors/matrices
+	//MatrixFunctions::multiply(2, 2, NodeEqnMatrix, ForceVector, tempVector);
+	
+	for (int k = 0; k < 16;k++)
+	{
+		LoadVector[k] = detJacob*tempVector[k];
+	}
 }
 
 calc_load_vector::~calc_load_vector()
@@ -123,11 +117,12 @@ double calc_load_vector::getLoadVector()
 	return LoadVector[16];
 }
 
-void calc_load_vector::printData(ostream &out) const 
+void calc_load_vector::printData(ostream &out) const
 {
-	//out << detJacob << endl;
+	out << endl << detJacob << endl << endl;
 	for (int j = 0; j < 16; j++)
 	{
-		out << LoadVector[j] << endl;
+		//out << LoadVector[j] << endl;
+		out << ForceVector[j] << endl;
 	}
 }
--- Adam's Project/calc_load_vector.h
+++ Adam's Project/calc_load_vector.h
@@ -11,27 +11,13 @@ class calc_load_vector
 protected:
 	int numPFs;
 	double detJacob;
-	double fx1 = 0;
-	double fy1 = 0;
-	double fx2 = 0;
-	double fy2 = 0;
-	double fx3 = 0;
-	double fy3 = 0;
-	double fx4 = 0;
-	double fy4 = 0;
-	double fx5 = 0;
-	double fy5 = 0;
-	double fx6 = 0;
-	double fy6 = 0;
-	double fx7 = 0;
-	double fy7 = 0;
-	double fx8 = 0;
-	double fy8 = 0;
+	int Node[8];
 	double **NodeEqnMatrix;
-	double *LoadVector;
+	double LoadVector[16];
+	double ForceVector[16];
 
 public:
-	calc_load_vector(ElementData &element, Controller &contoller, calc_B_matrix &calc_B_matrix, double zeta, double eta);
+	calc_load_vector(ElementData &element, Controller &contoller, calc_B_matrix & calc_B_matrix, double zeta, double eta);
 	~calc_load_vector();
 	double getLoadVector();
 	void printData(ostream &out) const;
